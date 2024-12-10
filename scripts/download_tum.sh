57c449763b73fe30eb080af2c9762c0b6d74e518
knightzzz9w <1315515299@qq.com>
2024-12-10 15:10:09 +0800

    first commit

---
+++ scripts/download_tum.sh
@@ -0,0 +1,8 @@
+mkdir -p datasets/tum
+cd datasets/tum
+wget -i https://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_desk.tgz
+tar -xvzf rgbd_dataset_freiburg1_desk.tgz
+wget -i https://vision.in.tum.de/rgbd/dataset/freiburg2/rgbd_dataset_freiburg2_xyz.tgz
+tar -xvzf rgbd_dataset_freiburg2_xyz.tgz
+wget -i https://vision.in.tum.de/rgbd/dataset/freiburg3/rgbd_dataset_freiburg3_long_office_household.tgz
+tar -xvzf rgbd_dataset_freiburg3_long_office_household.tgz
